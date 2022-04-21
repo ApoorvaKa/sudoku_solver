6ece288ad9df6d406e8c12d371b9c53e5413307f
Apoorva Kaushik <apoorva.kaushik@gmail.com>
2022-04-21 18:35:42 -0400

    Revert "added board and starter code"
    This reverts commit 21d4d2bcb2831b50dbc61bff3bb597b8f38d80e2.

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,20 +1,3 @@
 # Sudoku Solver
 # Apoorva Kaushik
 # Jerry Aska
-
-
-def get_board(filename):
-    with open(filename, 'r') as file:
-        board = []
-        for line in file:
-            row = line.strip().split()
-            board.append(row)
-    return board
-
-def main():
-
-    board = get_board("sudoku_random.txt")
-    for line in board:
-        print(line)
-
-main()
