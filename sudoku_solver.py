78f22001200e545b0e08f30e22b7f6840406e9ab
Apoorva Kaushik <apoorva.kaushik@gmail.com>
2022-04-27 16:14:40 -0400

    degree heuristic fixed

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -8,15 +8,17 @@ import copy
 
 DOMAIN = [str(i) for i in range(1, 10)]
 
-def print_board(sudoku_board):
+def string_board(sudoku_board):
     """
     Prints the sudoku board in required format
     """
-    print('\n'.join([' '.join(entry) for entry in sudoku_board]))
+    return '\n'.join([' '.join(entry) for entry in sudoku_board])
 
 
 def is_complete(state: list) -> bool:
-    """Checks if the sudoku board is complete
+    """
+    Checks if the sudoku board is filled in. 
+    Does NOT check legality since is_consistent is called for each cell at assignment
 
     Parameters:
         state (list): A nested list of a sudoku problem
@@ -29,9 +31,9 @@ def is_complete(state: list) -> bool:
             return False
     return True
 
-def degree_heuristic(sudoku_board: list, variables: list) -> tuple:
+def degree_heuristic(state: list, variables: list) -> tuple:
     """
-    Selects the variable with the most constraints
+    Selects the variable with the most unassigned neighbors in row, col, and box
 
     Parameters:
         sudoku_board (list): A nested list of a sudoku problem
@@ -40,25 +42,35 @@ def degree_heuristic(sudoku_board: list, variables: list) -> tuple:
     Returns:
         tuple: A tuple of the row and column of the empty cell
     """
-    max_count = -1
+    max_degree = -1
+    max_var = None
     for var in variables:
-        count = 0
-        for value in DOMAIN:
-            if is_consistent(sudoku_board, var[0], var[1], value):
-                count += 1
-        if count > max_count:
-            max_count = count
+        degree = 0
+        for i in range(9):
+            if state[var[0]][i] == '0':
+                degree += 1
+            if state[i][var[1]] == '0':
+                degree += 1
+        row_start = (var[0] // 3) * 3
+        col_start = (var[1] // 3) * 3
+        for i in range(row_start, row_start + 3):
+            for j in range(col_start, col_start + 3):
+                if state[i][j] == '0':
+                    degree += 1
+        if degree > max_degree:
+            max_degree = degree
             max_var = var
     return max_var
+    
 
-def minimum_remaining_value(board: list) -> tuple:
-    """Finds the empty cell in a sudoku board with fewest possible values
+def minimum_remaining_value(board: list) -> list:
+    """
+    Finds all of the empty cells in a sudoku board with fewest legal values possible
 
     Parameters:
         board (list): A nested list of a sudoku problem
-
     Returns:
-        tuple: A tuple of the row and column of the empty cell
+        list: A tuple of the row and column of the empty cell
     """
     min_count = 10
     variables = []
@@ -76,10 +88,13 @@ def minimum_remaining_value(board: list) -> tuple:
                     variables.append((i, j))
     return variables
 
-# This is the traditional backtrack selection. Just selects the first empty cell to fill
 def select_unassigned_variable(state: list) -> tuple:
     """
-    Return the first unasigned variable in the sudoku board
+    First runs the minimum remaining value heuristic and then degree heuristic to break ties
+    Parameters:
+        state (list): A nested list of a sudoku problem
+    Returns:
+        tuple: A tuple of the row and column of the empty cell
     """
     minimum_variables = minimum_remaining_value(state)
     variable = degree_heuristic(state, minimum_variables)
@@ -87,14 +102,14 @@ def select_unassigned_variable(state: list) -> tuple:
     return variable
 
 def is_consistent(state: list, row: int, col: int, num: int) -> bool:
-    """Checks if the given number is consistent with the given row and column
+    """
+    Checks if the given number is consistent with the given row and column
 
     Parameters:
         state (list): A nested list of a sudoku problem
         row (int): The row index
         col (int): The column index
         num (int): The number to be checked
-
     Returns:
         bool: True if the number is consistent, False otherwise
     """
@@ -111,7 +126,7 @@ def is_consistent(state: list, row: int, col: int, num: int) -> bool:
                 return False
     return True
 
-def backtrack(csp, state):
+def backtrack(csp: list, state: list) -> list:
     """
     Backtracking algorithm to solve sudoku
     """
@@ -123,7 +138,7 @@ def backtrack(csp, state):
         if is_consistent(state, var[0], var[1], value):
             state[var[0]][var[1]] = value
             print("----------------")
-            print_board(state)
+            print(string_board(state))
             if backtrack(csp, state):
                 return state
             state[var[0]][var[1]] = '0'
@@ -131,11 +146,10 @@ def backtrack(csp, state):
 
 def read_from_file(filename: str) -> list:
     """
-    File to state extractor
+    File to state extractor 
 
     Parameters:
         filename (str): A file name as a string
-
     Returns:
         list[list]: A nested list of a sudoku problem
     """
@@ -154,15 +168,17 @@ def main():
     temp = re.findall(r'\d+', filename)
     res = list(map(int, temp))
     print(res)
+    output_filename = "Output" + str(res[0]) + ".txt"
     
     ## Read from file and solve the sudoku
     csp = read_from_file(filename)
-    print_board(csp)
+    string_board(csp)
     state = copy.deepcopy(csp)
     final = backtrack(csp, state)
     print("-----------------")
     # print(final)
-    print_board(final)
+    with open(output_filename, "w") as out_file:
+        out_file.write(string_board(final))
 
 
 if __name__ == "__main__":
